24d78067cfc35ea5d32a38467a0c097ea1dcc2aa
aclementepa <aclementepa@gmail.com>
2021-06-10 21:37:35 -0400

    ko

--- index.php
+++ index.php
@@ -9,62 +9,134 @@
     <link type="text/css" rel="stylesheet" id="dark-mode-general-link">
     <link type="text/css" rel="stylesheet" href="main.css">
     <link type="text/css" rel="stylesheet" href="desktop.css">
+    <link type="text/css" rel="stylesheet" href="slider.css">
     <style lang="en" type="text/css" id="dark-mode-custom-style"></style>
     <style lang="en" type="text/css" id="dark-mode-native-style"></style>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap" rel="stylesheet">
     <style>
-        body {
-            background-image: none;
+    </style>
+    <style>
+        ::-webkit-scrollbar {            
+            width: 10px;
         }
+        ::-webkit-scrollbar-thumb{
+        background-color: #5b5d5a; 
+        }
+        ::-webkit-scrollbar-thumb:hover{
+        background: #2d2f2c; 
+        }
+        ::-webkit-scrollbar-thumb:active{
+        background: #000; 
+        }
+        ::-webkit-scrollbar-track {
+        background: #5b5d5a
+        }
+ 
+        /*
+        ::-webkit-scrollbar{
+            width:30px;
+          }
+          ::-webkit-scrollbar-track{
+            border-radius: 10px;
+          }
+          */
     </style>
 </head> 
 <body>
     <div class="row" style="margin:0;">        
-        <nav class="test-blur col-12" style="background-image: url(backgroundSmall.jpg); height: 800px; padding:0;">        
+        <nav class="test-blur col-12" style="background-color: #000;/* background-image: url(conquer.jpg); background-position:center; background-size: contain; background-repeat: no-repeat; height: 500px;*/ padding:0;">        
             <div id="nav-bar-container" class="container-fluid" style="width: 100%; opacity: .9; ">
                 <div class="container-fluid" style="padding:.5% 60px; ">
                     <div class="navbar navbar-expand-md" style="color: white;">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#" style="font-weight: 600; font-size: calc(1rem + 1.2vw); margin-left: 2.5%;">Clemente.Tech</a>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="font-size: 1.5rem; font-weight: 400;">
-                            <ul class="navbar-nav me-auto mb-0"  style="margin-left:70%; margin-right: auto;">
-                                <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Web Fun
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
+                        <a class="navbar-brand" href="https://clemente.tech" style="font-weight: 600; font-size: calc(1rem + 1.2vw); margin-left: 2.5%;">Clemente.Tech</a>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="font-size: calc(.1rem + 1.2vw); font-weight: 400;">
+                            <ul class="navbar-nav me-auto mb-0"  style="margin-left:65%; margin-right: auto;">
+                                <li class="nav-item dropdown d-none">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Web Fun
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color:green;">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Link</a>
+                                    <a class="nav-link" href="#">News</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Link</a>
+                                    <a class="nav-link" href="#">Projects</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#"><i class="fa-solid fa-right-to-bracket"></i></a>
+                                    <a class="nav-link" href="#">Ideas</a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    </li>
                             </ul>
                         </div>
+                        <div style="float: right; margin-top: 1%;">
+                            <a class="nav-link" href="#" style="font-size: 1.5rem; margin-top:.1rem;"><i class="fas fa-sign-in-alt"></i></a>
+                        </div>
+                        <div style="float: right; margin-top: 1%;">
+                            <label class="switch">
+                                <input type="checkbox" id="dark-mode"> 
+                                <span class="slider"></span>       
+                              </label>
+                        </div>
+
                     </div>
                     </div>
                 </div>
             </div>
         </nav>
     </div>
-    <div class="row" style="padding:1.5rem;">
-        <div class="blocks col-md-6" style="background-color: inherit;">
-            <h1 style="text-align: center; font-weight: 600;">Upcoming Features</h1>
-            <hr style="margin-left: auto; margin-right: auto; width: 75%; height: 1px; border: 1px #fff outset;">
-            <div class="container">
-                <ul style="list-style-type:none; text-align: left; font-size: 1.75rem; margin-left:10%;">
+    <div class="row" style="background-color: #000;  ">
+        <div id="backgroundCarousel" class="carousel slide" data-bs-ride="carousel" style="padding:0;margin-top: 5px;">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#backgroundCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#backgroundCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#backgroundCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#backgroundCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active" style="height: 550px;">
+                  <img src="ArnoldConquer.jpg" class="d-block w-100" alt="..." style="height: 550px;">
+                </div>
+                <div class="carousel-item " style="height: 550px;">
+                  <img src="happen.jpg" class="d-block w-100" alt="..." style="height: 550px;">
+                </div>
+                <div class="carousel-item"style="height: 550px;">
+                  <img src="batground.jpg" class="d-block w-100" alt="..." style="height: 550px;" >
+                </div>
+              <div class="carousel-item"style="height: 550px;">
+                <img src="background.jpg" class="d-block w-100" alt="..." style="height: 550px;" >
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#backgroundCarousel" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#backgroundCarousel" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <div class="row" style="padding:2.5rem; background-color: #eceae7; border-top: 1px solid #000;">
+        <div class="col-md-2 col-lg-6"></div>
+        <div class="card col-md-8 col-lg-6">
+            <div class="card-body">
+                <h3 class="card-title" style="text-align: center; font-weight: 600;">Upcoming Features</h3>
+                <h6 class="card-subtitle mb-2 text-muted d-none">Card subtitle</h6>
+                <ul class="card-text" style="list-style-type:none; text-align: center; font-size: 1rem; ">
                     <li><span>Daily Scheduling</span></li>
                     <li><span>Weekly Reminders</span></li>
-                    <li><span>Webscraping Upcoming News Sources - (jw.org, barcablaugranes, espn, goal.com, etc.)</span><a href="https://www.studytonight.com/python/web-scraping/exploring-beautifulsoup-methods">web scraping</a></li>
+                    <li><span>Webscrape News - </span><a href="https://www.studytonight.com/python/web-scraping/exploring-beautifulsoup-methods">web scraping</a></li>
                     <li><span>Workout/Fitness Tracking</span> - <a href="https://wger.de/en/software/api">wger</a></li>
                     <li><span>Lyric Auto-censoring</span> - <a href="https://developer.spotify.com/documentation/web-api/quick-start/">dev.spotify</a></li>
                     <li><span>Spotify Management</span> - <a href="https://github.com/thelinmichael/spotify-web-api-node">spotify.github</a></li>
@@ -73,15 +145,12 @@
                 </ul>
             </div>
         </div>
-        <div class="blocks col-md-6" style="background-color: inherit;">
-            <h1 style="text-align: center; font-weight: 600;">Installed Features</h1>
-            <hr style="margin-left: auto; margin-right: auto; width: 75%; height: 1px; border: 1px #fff outset;">
-            <div class="container mx-auto">
-                <ul style="width: 50%; list-style-type:none;">
-                    <li></li>
-                </ul>
-            </div>
-        </div>
+        <div class="col-lg-3">
+            <h1>test</h1>
+        </div>      
+    </div>
+    <div class="row">
+            <a class="nav-link" href="#"><i class="fa-solid fa-right-to-bracket"></i></a>
     </div>
     <div class="row d-none">
         <div class="col-12">
