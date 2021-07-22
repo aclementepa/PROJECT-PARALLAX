b1e2348494f08f72c2fd6138128ed388a64dca14
mis-howard <74306652+mis-howard@users.noreply.github.com>
2021-07-22 14:45:34 -0400

    update

---
+++ howard-psu-toxamp-copy.ps1
@@ -0,0 +1,2 @@
+Remove-Item -Path "C:\xampp\htdocs\PSU-college-signs\*" -Recurse
+Copy-Item -Path "C:\Development\Github\PSU-college-signs\*" -Destination "C:\xampp\htdocs\PSU-college-signs" -Recurse
