3f43b09c59f4e971d1700d97a6a5c06473ee31b5
mis-howard <74306652+mis-howard@users.noreply.github.com>
2021-10-06 13:13:57 -0400

    iter

--- Anagram.py
+++ Anagram.py
@@ -1,25 +1,65 @@
 import enchant
+from itertools import permutations
+import sys
+import math
 
-def Anagrams(word):
+def ConcatPermutations(letters):
+    return [''.join(i) for i in permutations(letters)]
+
+def Anagrams(letters):
     """ Generate all of the anagrams of a word. """ 
-    if len(word) < 2:
-        yield word
+    if len(letters) < 2:
+        yield letters
     else:
-        for i, letter in enumerate(word):
-            if not letter in word[:i]: #avoid duplicating earlier words
-                for j in Anagrams(word[:i]+word[i+1:]):
+        for i, letter in enumerate(letters):
+            if not letter in letters[:i]: # avoid duplicating earlier words
+                for j in Anagrams(letters[:i]+letters[i+1:]):
                     yield j+letter
 
+def Anagram_Permutations(letters):
+    return [''.join(i) for i in permutations(letters)]
 
-def Words(str):
+
+def Words(permutationsList):
     words = []
-    charSTR = [*str]
     d = enchant.Dict("en_US")
+    for word in permutationsList:
+        if d.check(word):
+            words.append(word)
+    return words
 
-line = args
+def UniqueList(listObj):
+    for elem in listObj:
+      if listObj.count(elem) > 1: 
+        listObj.remove(elem)
+    return listObj
 
-print("Anagrams:\n")
-for i in Anagrams("apple"):
+line = sys.argv[1]
+counter = 0
+
+print("Anagrams:")
+anagrams = Anagrams(line)
+for i in anagrams:
+    counter+=1
     print(i)
+print("\nNumber of Anagrams: " + str(counter))
+print("------------------------------------")
 
-print("Words from " + str)
+counter = 0
+print("Anagram Permutations:")
+anagrams = Anagram_Permutations(line)
+for i in anagrams:
+    counter+=1
+    print(i)
+print("\nNumber of Anagram Permutations: " + str(counter))
+counter = 0
+print("------------------------------------")
+counter = 0
+print("Words from " + line + ":")
+permutationsList = ConcatPermutations(line)
+wordList = Words(permutationsList)
+wordList = UniqueList(wordList)
+for word in wordList: 
+    print(word)
+    counter+=1
+print("Number of Words from " + line + ":" + str(counter))
